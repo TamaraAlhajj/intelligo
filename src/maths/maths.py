772104df60ef0053e4ae36c883a96a7fe81965cb
TamaraAlhajj <tamara.alhajj@carleton.ca>
2019-03-19 09:04:38 -0400

    todo: front end intergration, update from cli

---
+++ src/maths/maths.py
@@ -0,0 +1,142 @@
+from sympy import *
+from sympy.parsing.sympy_parser import parse_expr
+
+# from sympy.interactive import printing
+# printing.init_printing(use_latex=True)
+# pprint_try_use_unicode()
+
+n = symbols('n')
+
+
+
+def dominator(fn):
+    """ 
+    INPUT: f(n) \n
+    OUTPUT: O(g(n)) such that f(n) is bounded by g(n)
+    """
+
+    # turn input into math expression
+    f = parse_expr(fn)
+
+    # lowest possible bound, constant
+    if (f.is_constant()):
+        #g = parse_expr('1')
+        #lim = latex(Limit(f/g, n, oo))
+        g = '1'
+        return (g, None)
+
+    # otherwise check common bounds
+    bounds = ['log(n)', 'log(n)**2', 'sqrt(n)', 'n', 'n*log(log(n))',
+              'n*log(n)', 'n**2', 'n**3', '2**n', '3**n']
+    math_bounds = [parse_expr(e) for e in bounds]
+
+    for g in math_bounds:
+        # check if f(n) is bounded by g(n) as n --> inf
+        check = Limit(f/g, n, oo).doit()
+
+        if (check.is_constant() and (check != oo)):
+            g = str(g)
+            #lim = latex(Limit(f/g, n, oo))
+            return (g, check)
+
+    g = 'n!'
+    #lim = latex(Limit(f/g, n, oo))
+
+    return (g, None)
+
+
+def masters(a, b, k, i):
+
+    # T(n) = aT(n/b) + f(n) where a >= 1 and b > 1
+
+    msg = "Invalid input: "
+
+    if(a < 0 or None):
+        msg += "a must be a positive number. "
+    elif(b <= 1 or None):
+        msg += "b must be greater than 1. "
+    elif(k < 0 or None):
+        msg += "k must be at least 0. "
+    elif(i < 0 or None):
+        msg += "i must be at least 0. "
+    else:
+        c_critical = int(log(a,b))
+        print("The critical exponent is \n=log(# of subproblems)/log(relative problem size) \n= log(a)/log(b) \n= log base b of a \n= {}".format(c_critical))
+        fn = parse_expr("n**{} * log(n)**{}".format(k, i))
+        print("Recurrence: is {}T(n/{}) + {}".format(a, b, fn))
+        """ c = int(str(diff(fn,n))[:1])
+        print(c) """
+        if(c_critical > k):
+            # case 1
+            msg = "Case 1: \nT(n) = Θ(n^{})".format(c_critical)
+        if(c_critical == k):
+            # case 2
+            expr = parse_expr("Θ(n**{} * (log(n))**{})".format(c_critical, i+1))
+            msg = "Case 2: \nT(n) = {}".format(expr)
+        if(c_critical < k):
+            # case 3
+            msg = "Case 3: \nT(n) = {}".format(fn)
+        
+    return msg
+
+
+def run():
+
+    testing = True
+
+    while(testing):
+
+        user = input("Press 1 for Big-O tool. Press 2 for Master's Method Tool. \n > ")
+        if(user == '1'):
+            try:
+                f = input("Please input your function using python's math format. \nf(n) = ")
+                ans = dominator(f)
+                g = ans[0]
+                const = ans[1]
+
+                print("-"*20 + "OUTPUT" + "-"*20)
+                if(const == None):
+                    if(g == '1'):
+                        print("The function {} is not dependent on n. \nThus, The run time is constant. \nIn other words, f(n) is O({})".format(f, g))
+                    else:
+                        print("The function {} is bound by {}. Thus, f(n) is O({})".format(f, g, g))
+                else:
+                    print("The limit of ({})/{} as n grows to infinity is {}. \nThus, fn is bound by {}. \nIn other words, f(n) is O({})".format(f, g, const, g, g))
+            except SyntaxError:
+                print("Invalid Syntax given for f(n)")
+        elif(user == '2'):
+            try:
+                print("For the recurrence T(n) = aT(n/b) + Θ(n^k * (logn)^i)")
+                tn = input("Seperated by spaces, please input a, b, k, i : ")
+                lst = tn.split(' ', 3)
+                ints = [int(x) for x in lst]
+
+                msg = masters(ints[0], ints[1], ints[2], ints[3])
+
+                print(msg)
+            except IndexError:
+                print("Invalid input of variables for T(n)")
+        else:
+            print("Invalid input")
+
+        check = input("-"*40 + "\nWant to try again? y/n ")
+        if(check == 'n'):
+            testing = False
+
+run()
+
+###testing###
+"""
+f = '4'
+f = '5+10'
+f = 'n'
+f = '2*n + 4'
+f = 'n*(n+1)/2'
+f = 'log(n) + n**2'
+f = 'n*log(log(n))'
+f = '2**n + n*log(n) + 4*n + 52'
+f = '3**n - 2'
+f = 
+f = 'n**3 + log(n) - 5'
+f = 'n!'
+"""
