449e00ef647133b9e38b6f2dc407c54aa27fb42e
TamaraAlhajj <tamaraalhajjcs@gmail.com>
2023-12-03 23:24:50 -0500

    rename prod vars

--- intelligo/settings.py
+++ intelligo/settings.py
@@ -16,8 +16,8 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-DEBUG = bool(os.getenv('DJANGO_DEBUG', default=False))
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = bool(os.getenv('DEBUG', default=False))
 
 ALLOWED_HOSTS = [
     'intelligo.up.railway.app',
